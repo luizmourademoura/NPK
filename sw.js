1574e0596b1e677049ae36135fddc9574e5cb23a
luizmourademoura <78830766+luizmourademoura@users.noreply.github.com>
2025-05-05 11:57:58 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,11 @@
-cconst CACHE_NAME = "joia-cache-v1";
-const urlsToCache = [
-  "./index.html",
-  "./manifest.json",
-  "./style.css",
-  "./icon-72x72.png",
-  "./icon-96x96.png",
-  "./icon-128x128.png",
-  "./icon-144x144.png",
-  "./icon-152x152.png",
-  "./icon-192x192.png",
-  "./icon-384x384.png",
-  "./icon-512x512.png"
-];
+<link rel="manifest" href="manifest.json">
+<link rel="icon" href="icons/icon-192.png" type="image/png">
+<meta name="theme-color" content="#2e7d32">
 
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
-});
+<script>
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+      .then(() => console.log('Service Worker registrado com sucesso'))
+      .catch(error => console.error('Erro ao registrar o Service Worker:', error));
+  }
+</script>
